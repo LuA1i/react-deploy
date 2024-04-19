e3bfd6fd0362a88764a0bf64a8dc559ae6bb6384
LuA1i <117823970+LuA1i@users.noreply.github.com>
2024-04-19 22:52:43 +0100

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
+import Contacts from './Components/Contacts/Contacts'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
@@ -20,6 +22,9 @@ const App = () => {
         <Campus />
         <Title subTitle="TESTIMONIALS" title="What Student Say" />
         <Testimonials />
+        <Title subTitle="Contact US" title="Get In Touch" />
+        <Contacts />
+        <Footer />
       </div>
     </div>
   )
